6f6c31d2ecd5c1e847c0f0bd349a44aff8404beb
Doug Bennett <doukl54491@maricopa.edu>
2022-02-21 20:49:51 -0700

    Add files via upload

---
+++ NumberReversal/ConsoleApp1/NumReversalMethods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberReversal
+{
+    public static class SequenceOfNumbers
+    {
+        private static Stack<int> currentNumbers = new Stack<int>();
+        private static List<int> numbers = new List<int>();
+
+        public static void AddNumbers()
+        {
+            Console.Write("Enter amount of numbers to reverse: ");
+            int numbersLength = int.Parse(Console.ReadLine());
+
+            if (numbersLength <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Numbers length cannot negative or zero. " +
+                    "You have to enter at least 1 number and up to 15 numbers");
+            }
+
+            for (int i = 0; i < numbersLength; i++)
+            {
+                Console.Write("Enter number: ");
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+        }
+
+        public static void ReverseNumbers()
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                currentNumbers.Push(numbers[i]);
+            }
+
+            numbers.Clear();
+
+            while (currentNumbers.Count > 0)
+            {
+                numbers.Add(currentNumbers.Pop());
+            }
+        }
+
+        public static void PrintNumbers()
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
